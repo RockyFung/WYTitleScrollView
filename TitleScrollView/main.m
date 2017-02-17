6a69d53b528825696e50a9d4a385c370009512dc
rockyMni <fengjian0213@126.com>
2017-02-17 11:15:21 +0800

    Initial Commit

---
+++ TitleScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TitleScrollView
+//
+//  Created by rocky on 2017/2/17.
+//  Copyright © 2017年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
